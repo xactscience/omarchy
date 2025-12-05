1b328b83f2e0c78308c2f65846337757f57065eb
xactscience <xactscience@pm.me>
2025-12-04 20:47:21 -0500

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -20,7 +20,7 @@ echo -e "\n$ansi_art\n"
 sudo pacman -Syu --noconfirm --needed git
 
 # Use custom repo if specified, otherwise default to basecamp/omarchy
-OMARCHY_REPO="${OMARCHY_REPO:-basecamp/omarchy}"
+OMARCHY_REPO="${OMARCHY_REPO:-xactscience/omarchy}"
 
 echo -e "\nCloning Omarchy from: https://github.com/${OMARCHY_REPO}.git"
 rm -rf ~/.local/share/omarchy/
